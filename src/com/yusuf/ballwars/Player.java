75fd8351ab913860f5c2f03d8a888b759e256971
yu5uf <ka1f4j7@gmail.com>
2026-07-06 22:56:46 +0300

    Add Player Angle attribute to Player class
    Updated Player class to include Player Angle attribute and modified constructor.

--- src/com/yusuf/ballwars/Player.java
+++ src/com/yusuf/ballwars/Player.java
@@ -2,11 +2,13 @@ package com.yusuf.ballwars;
 
 public class Player {
     public int id;
+    public float pang;
     public String isim;
     public Ball top;
 
-    public Player(int id, String isim, Ball top) {
+    public Player(int id, float pang, String isim, Ball top) {
         this.id = id;
+        this.pang = pang
         this.isim = isim;
         this.top = top;
     }
